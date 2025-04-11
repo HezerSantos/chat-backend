58db9a3c87eb5add2a05a3506fef6b9b3a873f27
HezerSantos <HezerSantos24@gmail.com>
2025-04-11 01:28:11 -0500

    Added the friends and notifications page

--- controllers/users/userController.js
+++ controllers/users/userController.js
@@ -38,3 +38,215 @@ exports.createUser = [
         }
     }
 ]
+
+exports.getUsers = async(req, res, next) => {
+    try{
+        const users = await prisma.user.findMany({
+            select: {
+                id: true,
+                username: true
+            }
+        })
+
+        const friends = await prisma.friend.findMany({
+            where:{
+                userId: req.user.id
+            }
+        })
+
+        const receiverIds = await prisma.request.findMany({
+            where: {
+                senderId: req.user.id
+            }
+        })
+
+
+        let filteredUsers = users.filter(user => user.id !== req.user.id)
+
+        if (friends){
+            const friendSet = new Set(friends.map(friend => friend.friendId))
+            filteredUsers = filteredUsers.filter(user => !friendSet.has(user.id))
+        }
+
+        if(receiverIds){
+            const receiverSet = new Set(receiverIds.map(id => id.receiverId))
+            filteredUsers = filteredUsers.filter(user => !receiverSet.has(user.id))
+        }
+
+        return res.json({
+            users: filteredUsers
+        })
+    } catch(error){
+        next(error)
+    }
+}
+
+
+exports.sendRequest = async(req, res, next) => {
+    try{
+        const { userId } = req.params
+
+        const senderId = req.user.id
+
+        await prisma.request.create({
+            data: {
+                senderId: senderId,
+                receiverId: parseInt(userId)
+            }
+        })
+
+        return res.json({
+            message: "Request Sent Successfully"
+        })
+    } catch(error){
+        next(error)
+    }
+}
+
+exports.deletePending = async(req, res, next) => {
+    try{
+        const { userId: receiverId } = req.params
+        const senderId = req.user.id
+
+        await prisma.request.delete({
+            where: {
+              senderId_receiverId: {
+                senderId: senderId,
+                receiverId: parseInt(receiverId),
+              },
+            },
+        })
+
+        return res.json("Delted Pending Request")
+    } catch(error){
+        next(error)
+    }
+}
+
+exports.deleteReceived = async(req, res, next) => {
+    try{
+        const  { userId: senderId } = req.params
+        const receiverId = req.user.id
+
+        await prisma.request.delete({
+            where: {
+              senderId_receiverId: {
+                senderId: parseInt(senderId),
+                receiverId: receiverId,
+              },
+            },
+        })
+
+        return res.json("Deleted Received Request")
+    } catch(error){
+        next(error)
+    }
+}
+
+exports.getRequests = async(req, res, next) => {
+    try{
+        const requests = await prisma.user.findUnique({
+            where: {
+              id: req.user.id,
+            },
+            include: {
+              sentRequests: {
+                include: {
+                  receiver: {
+                    select: { username: true },
+                  },
+                },
+              },
+              receivedRequests: {
+                include: {
+                  sender: {
+                    select: { username: true },
+                  },
+                },
+              },
+            },
+          });
+
+        
+        return res.json({
+            requests
+        })
+    } catch(error){
+        next(error)
+    }
+}
+
+exports.addFriend = async(req, res, next) => {
+    try{
+        const senderId = parseInt(req.params.userId, 10);
+        const receiverId = req.user.id
+
+        await prisma.friend.createMany({
+            data: [
+                { userId: receiverId, friendId: parseInt(senderId) },
+                { userId: parseInt(senderId), friendId: receiverId }
+            ]
+        });
+
+        // Delete any pending friend requests in either direction
+        await prisma.request.deleteMany({
+            where: {
+                OR: [
+                    { senderId , receiverId },
+                    { senderId: receiverId, receiverId: parseInt(senderId) }
+                ]
+            }
+        });
+
+        return res.json("Friend Created")
+    } catch(error){
+        next(error)
+    }
+}
+
+
+exports.getFriends = async(req, res, next) => {
+    try{
+        const friends = await prisma.user.findUnique({
+            where: {
+                id: req.user.id
+            },
+            select: {
+                friendsAsUser: {
+                    include: {
+                        friend: {
+                            select: {
+                                username: true
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        return res.json({
+            friends: friends
+        })
+    }catch(error){
+        next(error)
+    }
+}
+
+exports.deleteFriend = async(req, res, next) => {
+    try{
+        const friendId = parseInt(req.params.userId)
+
+        await prisma.friend.deleteMany({
+            where: {
+                OR: [
+                    { userId: req.user.id, friendId: friendId },
+                    {userId: friendId, friendId: req.user.id }
+                ]
+            }
+        })
+
+        return res.json("Deleted Friend")
+    } catch(error){
+        next(error)
+    }
+}
--- routes/users/userRouter.js
+++ routes/users/userRouter.js
@@ -1,10 +1,22 @@
 const { Router } = require('express')
-const { createUser } = require('../../controllers/users/userController')
+const { createUser, getUsers, sendRequest, getRequests, deletePending, deleteReceived, addFriend, getFriends, deleteFriend } = require('../../controllers/users/userController')
+const passport = require('passport')
 
 const userRouter = Router()
 
-
+userRouter.get("/", passport.authenticate("jwt", {session: false}), getUsers)
 userRouter.post("/", createUser)
 
+
+userRouter.post("/:userId/friends/request", passport.authenticate("jwt", {session: false}), sendRequest)
+
+userRouter.delete("/:userId/friends/request/pending", passport.authenticate("jwt", {session: false}), deletePending)
+userRouter.delete("/:userId/friends/request/received", passport.authenticate("jwt", {session: false}), deleteReceived)
+
+userRouter.get("/friends/request", passport.authenticate("jwt", {session: false}), getRequests)
+
+userRouter.post("/:userId/friends", passport.authenticate("jwt", {session: false}), addFriend)
+userRouter.get("/friends", passport.authenticate("jwt", {session: false}), getFriends)
+userRouter.delete("/:userId/friends", passport.authenticate("jwt", { session: false}), deleteFriend)
 
 module.exports = userRouter
