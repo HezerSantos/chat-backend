8f3bdd25b6b9de838940644438f23d78f13b6bc7
HezerSantos <HezerSantos24@gmail.com>
2025-04-27 14:40:42 -0500

    Added frontend domain to allowed origins

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const allowedOrigins = [
   "http://localhost:5173", 
   "http://127.0.0.1:5173", 
-  
+  "https://chat-frontend-production-c485.up.railway.app"
 ];
 
 const corsOptions = {
